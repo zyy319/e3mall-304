6193377331f7e358c913c2f68f3afe535aca7283
zyy <1129095016@qq.com>
2018-04-23 00:15:08 +0800

    添加manager工程

---
+++ e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
@@ -0,0 +1,20 @@
+package cn.e3mall.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cn.e3mall.mapper.TbItemMapper;
+import cn.e3mall.pojo.TbItem;
+import cn.e3mall.service.ItemService;
+
+@Service
+public class ItemServiceImpl implements ItemService {
+
+	@Autowired
+	private TbItemMapper itemMapper;
+	
+	public TbItem getItemById(long itemId) {
+		return itemMapper.selectByPrimaryKey(itemId);
+	}
+
+}
